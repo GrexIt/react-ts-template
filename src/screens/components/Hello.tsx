baab6b7f71e8c4165d8a54b06aca2a4e7d2c016b
Anurag Maherchandani <anurag@grexit.com>
2019-10-22 18:44:43 +0530

    adding circleci

--- src/screens/components/Hello.tsx
+++ src/screens/components/Hello.tsx
@@ -4,7 +4,7 @@ import ThemeContext from '../../contexts/ThemeContext';
 export const Hello = () => {
   const { currentTheme } = React.useContext(ThemeContext);
   const { fontSize, backgroundColor } = currentTheme;
-  return <p style={{ ...styles.wrapper, backgroundColor, fontSize }}>Hello from MainScreen!</p>;
+  return <p style={{ ...styles.wrapper, backgroundColor, fontSize }}>Hello from MainScreen!!</p>;
 };
 
 const styles = {
